95fca183cb44d0d0070cb96a5a4f7e74628a4f35
vagif-guliyev <vaguliyev@gmail.com>
2022-12-18 19:52:48 +0400

    first commit

---
+++ firstproject/FirstProject.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.firstproject;
+
+/**
+ *
+ * @author vaqifquliyev
+ */
+public class FirstProject {
+
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 10;
+        int z = x + y;
+        System.out.println(z);
+    }
+}
