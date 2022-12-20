caf5ea1b94594bda959c825205d05fcf3f6b2358
vagif-guliyev <vaguliyev@gmail.com>
2022-12-20 20:53:08 +0400

    Add for task 3.

--- firstproject/FirstProject.java
+++ firstproject/FirstProject.java
@@ -1,7 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.firstproject;
 
 import java.util.Scanner;
@@ -13,13 +12,24 @@ import java.util.Scanner;
 public class FirstProject {
 
     public static void main(String[] args) {
+
+        /*
+        1: 4 bos 1 *
+        2: 3 bos 2 *
+        3: 2 bos 3 *
+        4: 1 bos 4 *
+        5: 0 bos 5 *
+        */
         
-        for (int row = 1; row <= 5; row++) {
-            for (int i=0; i < row; i++) {
+        for (int i=0; i<5; i++) {
+            for (int j=0; j<4-i; j++) {
+                System.out.print(" ");
+            }
+            for (int j=0; j<i; j++) {
                 System.out.print("*");
             }
             System.out.println("");
         }
-        
+
     }
 }
