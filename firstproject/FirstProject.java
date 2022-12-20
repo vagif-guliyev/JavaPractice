c3b182db69951a53251f8afc06f8e91152371e99
vagif-guliyev <vaguliyev@gmail.com>
2022-12-20 21:03:00 +0400

    Add for task 4.

--- firstproject/FirstProject.java
+++ firstproject/FirstProject.java
@@ -13,21 +13,14 @@ public class FirstProject {
 
     public static void main(String[] args) {
 
-        /*
-        1: 4 bos 1 *
-        2: 3 bos 2 *
-        3: 2 bos 3 *
-        4: 1 bos 4 *
-        5: 0 bos 5 *
-        */
-        
-        for (int i=0; i<5; i++) {
-            for (int j=0; j<4-i; j++) {
-                System.out.print(" ");
-            }
-            for (int j=0; j<i; j++) {
-                System.out.print("*");
-            }
+        for (int i=0; i<4; i++) {
+            for (int j=0; j<8; j++) {
+                if (j==3 || j==4) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("=");
+                }
+               }
             System.out.println("");
         }
 
