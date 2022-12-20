b5d10eadde76c19031b4b2512515f0a88d4d2fc0
vagif-guliyev <vaguliyev@gmail.com>
2022-12-20 20:35:23 +0400

    Add for task 2

--- firstproject/FirstProject.java
+++ firstproject/FirstProject.java
@@ -14,9 +14,9 @@ public class FirstProject {
 
     public static void main(String[] args) {
         
-        for (int c = 0; c < 5; c++) {
-            for (int r = 0; r < 10; r++) {
-            System.out.print("*");
+        for (int row = 1; row <= 5; row++) {
+            for (int i=0; i < row; i++) {
+                System.out.print("*");
             }
             System.out.println("");
         }
